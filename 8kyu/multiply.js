9a0e8f54c7dce7bcc4ba471890023f3923eb0531
Kim Vyacheslav <kimv.oppa@gmail.com>
2025-12-17 23:27:43 +0900

    1 task done

---
+++ 8kyu/multiply.js
@@ -0,0 +1,22 @@
+// ❓Instructions:
+// This code does not execute properly. Try to figure out why.
+
+// Этот код не выполняется должным образом. Попытайтесь выяснить, почему.
+
+// 🐣TESTS
+// function multiply(a, b){
+//   a * b
+// }
+
+// ✅Solution:
+
+function multiply(a, b) {
+  let result = (a * b);
+  return result;
+}
+
+const result_1 = multiply(5,7)
+const result_2 = multiply( 10 , 3)
+console.log(result_1)
+console.log(result_2)
+
