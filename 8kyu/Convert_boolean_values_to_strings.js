14d40b6487e1aee8fddc2ed0a8832f3a1cea1d72
Kim Vyacheslav <kimv.oppa@gmail.com>
2025-12-29 20:12:41 +0900

    done

---
+++ 8kyu/Convert_boolean_values_to_strings.js
@@ -0,0 +1,28 @@
+// ❓Description:
+// Complete the method that takes a boolean value and return a "Yes" string for true, or a "No" string for false.
+
+// ОПИСАНИЕ:
+// Завершите метод, который принимает булево значение и возвращает строку «Yes» для true или строку «No» для false.
+
+// 🔵 // Notes;
+// Примечания:
+
+// 🐣TESTS
+
+// ✅Solution:
+function boolToWord(Truth) {
+  if (Truth) {
+    return ("Yes")
+  } else {
+    return ("No")
+  }
+}
+
+console.log(boolToWord(true)); //true
+console.log(boolToWord(false)); // false
+
+
+
+
+
+
